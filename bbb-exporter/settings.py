c1f7bbf5b5943c3f1135665c7699eefa19890e41
greenstatic <gregor@krmelj.xyz>
2020-05-18 18:22:37 +0200

    bump version to v0.4.0-preview

--- bbb-exporter/settings.py
+++ bbb-exporter/settings.py
@@ -3,9 +3,9 @@ import api_lib
 from helpers import validate_api_base_url, validate_buckets
 
 MAJOR = 0
-MINOR = 3
+MINOR = 4
 BUGFIX = 0
-INFO = ""
+INFO = "preview"
 
 VERSION = "{}.{}.{}".format(MAJOR, MINOR, BUGFIX)
 if INFO:
