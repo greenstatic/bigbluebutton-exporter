d136ff3858b363f4a2807befee60468ba02e775c
Gregor Krmelj <gregor@krmelj.xyz>
2020-09-15 10:55:36 +0200

    bump release in settings.py

--- bbb-exporter/settings.py
+++ bbb-exporter/settings.py
@@ -4,7 +4,7 @@ from helpers import validate_api_base_url, validate_buckets, str_to_bool_or_none
 
 MAJOR = 0
 MINOR = 5
-BUGFIX = 1
+BUGFIX = 2
 INFO = ""
 
 VERSION = "{}.{}.{}".format(MAJOR, MINOR, BUGFIX)
